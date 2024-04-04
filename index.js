28134f90d881f2f24ebc324a19b6ea34c447e560
Snyder111 <43899601+Snyder111@users.noreply.github.com>
2024-04-04 04:38:17 +0000

    BasicNode

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('Hello World');
+const { EventEmitter } = require('events');
+const { readFile, readFileSync } = require('fs');
+const express = require('express');
+const { request } = require('http');
+
+const app = express();
+
+app.get('/', (request, response) => {
+
+    readFile('./home.html', 'utf8', (err, html) => {
+
+        if(err) {
+            response.status(500).send('Sorry, out of order :(');
+        }
+
+        response.send(html);
+    })
+
+});
+
+app.listen(process.env.PORT || 3000, () => console.log('App available on http://localhost:3000'));
